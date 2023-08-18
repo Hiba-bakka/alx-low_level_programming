d72c1669ef395760ecee84bb84223933adbd55ea
Hibabakka <hbbakka@gmail.com>
2023-08-17 17:55:29 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplier two integer
+ * @a: int to mult
+ * @b: int to mult
+ *
+ * Return: retirn result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
