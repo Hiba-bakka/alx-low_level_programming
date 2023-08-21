4f6a7b3e3142d7ce00bf0453314bfe24ddc66f2e
Hibabakka <hbbakka@gmail.com>
2023-08-21 15:07:55 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function that returns string length
+ * @s:string
+ * 
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int count;
+	for (count=0; *s !='\0'; s++)
+
+		count++;
+		return (count);
+}
