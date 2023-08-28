8627367049b775f822f71cbfded9e4fe53d6d0f9
Hibabakka <hbbakka@gmail.com>
2023-08-28 07:28:00 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,14 +8,14 @@
  * @n: number of character to be replaced
  * Return: Pointer to memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	 int i;
-	
-	for (i=0; i > 0 ; i--)
+	int i;
+
+	for (i = 0; n > 0; i++)
 	{
 		s[i] = b;
+		n--;
 	}
-	return(s);
+	return (s);
 }
