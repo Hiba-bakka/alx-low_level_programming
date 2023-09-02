ee4176d346e6777542459d03e1a3dd149f1b6b7f
Hibabakka <hbbakka@gmail.com>
2023-09-01 18:10:17 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,10 @@
  *
  * return always 0
  */
-
-int main(int argc _attribut_((unused)), char *argv[])
+int main(int argc, char **argv)
 {
-	printf("%s\n", *argv);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
 	return (0);
 }
