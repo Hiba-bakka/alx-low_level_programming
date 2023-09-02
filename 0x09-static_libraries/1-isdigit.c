53e0ff1c8beb5004050cad542182e95dd85542a1
Hibabakka <hbbakka@gmail.com>
2023-09-01 17:38:54 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - to check for digit
+ *
+ * @c: character to determine
+ *
+ * Return: 1 if digit 0 if not
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+		return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function that returns string length
+ * @s:string
+ * description:a function that returns the length of a string
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (*s != '\0')
+	{
+		n++;
+		s++;
+	}
+	return (n);
+}
