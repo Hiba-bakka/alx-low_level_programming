17008c61639c945b93000d85996fcab6177afde3
Hibabakka <hbbakka@gmail.com>
2023-10-05 12:59:58 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+nclude "main.h"
+
+/**
+ *  *read_textfile-function that reads a text file and prints it to the POSIX
+ *   *@filename: file
+ *    *@letters: file
+ *     *
+ *      *Return:z
+ *       */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+		ssize_t x, y, z;
+			char *no;
+
+				x = open(filename, O_RDONLY);
+
+					if (x == -1)
+							{
+										return (0);
+											}
+						no = malloc(sizeof(char) * letters);
+
+							y = read(x, no, letters);
+								z = write(STDOUT_FILENO, no, y);
+									free(no);
+										close(x);
+											return (z);
+}
