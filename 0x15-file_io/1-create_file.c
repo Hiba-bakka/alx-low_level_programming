2fa7e957a7f8b23bce0a287f4b3cce746037b166
Hiba-bakka <138728531+Hiba-bakka@users.noreply.github.com>
2023-10-05 17:21:58 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,32 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  *create_file - function that creates a file.
- *   *@filename: pointer
- *    *@text_content: pointer
- *     *
- *      *Return:1
- *       */
+ *create_file - function that creates a file.
+ *@filename: pointer
+ *@text_content: pointer
+ *
+ *Return:1
+ */
 
 int create_file(const char *filename, char *text_content)
 {
-		int x, y, z = 0;
+	int x, y, z = 0;
 
-		if (filename == NULL)
-			return (-1);
+if (filename == NULL)
+return (-1);
 
-			if (text_content != NULL)
-					{
-							for (z = 0; text_content[z];)
-									z++;
-								}
-				x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-					y = write(x, text_content, z);
+	if (text_content != NULL)
+	{
+	for (z = 0; text_content[z];)
+	z++;
+	}
+	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	y = write(x, text_content, z);
 
-						if (x == -1 || y == -1)
-									return (-1);
+	if (x == -1 || y == -1)
+		return (-1);
 
-							close(z);
+	close(z);
 
-								return (1);
+	return (1);
 }
